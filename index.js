2d7f7bcbd743344663b423e90117d84cc0840190
Jkim695 <jkim695@jh.edu>
2024-08-29 12:32:30 -0400

    Created JS function and linked script to index.html

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 // Console is mapped to terminal
 // typescript is a superset of javascript
-const fallAsleepTime = new Date();
-fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
-const config = {
-    timeStyle:"short"
-};
-console.log("You will fall asleep at ", fallAsleepTime.toLocaleTimeString("en-US", config));
 
-const wakeUpTime = new Date(fallAsleepTime);
-wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 19);
-const wakeUpTimes = []
-
-console.log("Wake up at: ", wakeUpTime.toLocaleTimeString("en-US", config));
-
-for (let i = 1; i <= 6; i++) {
-    wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
-    const wakeUpTimeString = wakeUpTime.toLocaleTimeString("en-US", config);
-    wakeUpTimes.push(wakeUpTimeString);
+function calcWakeUpTimes() {
+    const fallAsleepTime = new Date();
+    fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
+    const config = {
+        timeStyle:"short"
+    };
+    console.log("You will fall asleep at ", fallAsleepTime.toLocaleTimeString("en-US", config));
+    
+    const wakeUpTime = new Date(fallAsleepTime);
+    wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 19);
+    const wakeUpTimes = []
+    
+    console.log("Wake up at: ", wakeUpTime.toLocaleTimeString("en-US", config));
+    
+    for (let i = 1; i <= 6; i++) {
+        wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
+        const wakeUpTimeString = wakeUpTime.toLocaleTimeString("en-US", config);
+        wakeUpTimes.push(wakeUpTimeString);
+    }
+    console.log(`Wake up times are`, wakeUpTimes);    
 }
-console.log(`Wake up times are`, wakeUpTimes);
 
+const calcBtn = document.getElementById("calc-btn");
+calcBtn.onclick = calcWakeUpTimes;
